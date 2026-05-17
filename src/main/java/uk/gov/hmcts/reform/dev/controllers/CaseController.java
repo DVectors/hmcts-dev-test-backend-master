a0dfcc6a4fc12d4089876cdaace8e8beb48c5d43
DVectors <drvectorartworks@gmail.com>
2026-05-17 15:00:50 +0100

    [ADD] Added function for display of tasks data.

--- src/main/java/uk/gov/hmcts/reform/dev/controllers/CaseController.java
+++ src/main/java/uk/gov/hmcts/reform/dev/controllers/CaseController.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.dev.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.dev.models.ExampleCase;
 
@@ -17,5 +18,10 @@ public class CaseController {
         return ok(new ExampleCase(1, "ABC12345", "Case Title",
                                   "Case Description", "Case Status", LocalDateTime.now()
         ));
+    }
+
+    @GetMapping(value = "/tasks", produces = "application/json")
+    public ResponseEntity<String> getAllTasks() {
+        return ok("This endpoint will return a list of tasks associated with the case.");
     }
 }
